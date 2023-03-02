79becd14630a9406377020b6f71462818e610bb6
Jorge Alan <jorge.alann@hotmail.com>
2023-03-02 14:50:42 -0300

    DML - Insert, Update, Delete

--- ConsultasMysql.sql
+++ ConsultasMysql.sql
@@ -1,16 +1,17 @@
+USE consultasmysql;
+
 /*INSERT*/
 
 INSERT INTO tipos_produtos (descricao) VALUES ('Eletronico');
 INSERT INTO produtos (descricao,preco,id_tipo_produtos) VALUES ('Notebook HP', 2200, 1);
 
+/*UPDATE*/
+UPDATE tipos_produtos SET descricao = 'Eletronicos' WHERE id = 1;
+UPDATE produtos SET descricao = 'Notebook HP I9', preco = 2500 WHERE id = 5;
 
-INSERT INTO tipos_produtos (descricao) VALUES ('Moveis');
-select * FROM tipos_produtos;
-
-INSERT INTO produtos (descricao, preco, id_tipo_produtos) VALUES ('Cadeira TOP', 80 , 3); 
-
-DELETE FROM tipos_produtos WHERE id = 2;
-
+-- DELETE
+DELETE FROM produtos WHERE id = 8;
+DELETE FROM tipos_produtos WHERE id = 3;
 
 
 
